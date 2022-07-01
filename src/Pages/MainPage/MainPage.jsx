f607c33912a8c93d99b1e5c5407c2e92ca15a398
chathurperera <chathuraperera007@gmail.com>
2022-07-01 15:51:10 +0530

    footer section added

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AboutSection from '../../Components/AboutSection/AboutSection';
 import CustomCursor from '../../Components/CustomCursor/CustomCursor';
+import Footer from '../../Components/Footer/Footer';
 import HeroSection from '../../Components/HeroSection/HeroSection';
 import NavBar from '../../Components/Navbar/Navbar';
 import WorkSection from '../../Components/WorkSection/WorkSection';
@@ -13,6 +14,7 @@ const MainPage = () => {
       <HeroSection />
       <AboutSection />
       <WorkSection />
+      <Footer />
     </div>
   )
 }
