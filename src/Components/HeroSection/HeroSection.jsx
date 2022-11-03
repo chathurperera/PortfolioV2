de716e38ab3a237040a2942f18029c55f40a7764
chathurperera <chathuraperera007@gmail.com>
2022-11-03 11:58:14 +0530

    curved text added

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './HeroSection.module.scss';
 import scrollTriggerArrow from '../../Assets/scrolldownArrow.png'
+import CircularText from '../../Assets/Circular Text.png'
 export default function HeroSection() {
   return (
     <section className={styles.heroSection}>
@@ -8,10 +9,14 @@ export default function HeroSection() {
         <h1 className={`${styles.heroSectionTitle} `}>
           Developer devoted to crafting <i>intuitive</i> web experiences
         </h1>
-      <p className={`${styles.scrollTrigger} ${styles.fadeInBottomTrigger}`}>
+        <div className={styles.circularText}>
+          <img src={CircularText} className={styles.circularTextImage} alt="Circular Text" />
+        <img className={styles.arrowIcon} src={scrollTriggerArrow} alt="" />
+        </div>
+      {/* <p className={`${styles.scrollTrigger} ${styles.fadeInBottomTrigger}`}>
         <span className={`${styles.bgAnimate} ${styles.bgAnimatedText}`}>scroll to discover</span>
         <img className={styles.bounce} src={scrollTriggerArrow} alt="" />
-      </p>
+      </p> */}
       </div>
     </section>
   )
